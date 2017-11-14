87d90933c83d4558de3c524278a2f3b22d8c2589
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2017-11-14 19:06:57 +0545

    add youtube search input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY ="AIzaSyC9xmW8y9sU8YCZrZzNLm0zw65KNkUvqNQ"
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = ()=>{
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+const rootDir = document.querySelector(".container")
+ReactDOM.render(<App />, rootDir);
