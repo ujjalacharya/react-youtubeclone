ca05fb46bb37aed480a78d368aee5a36bfb58ea2
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2017-11-17 20:41:11 +0545

    added youtube search

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from "./components/video_list";
+const API_KEY ="AIzaSyC9xmW8y9sU8YCZrZzNLm0zw65KNkUvqNQ";
 
-const API_KEY ="AIzaSyC9xmW8y9sU8YCZrZzNLm0zw65KNkUvqNQ"
 
-const App = ()=>{
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {videos: []}
+
+        YTSearch({key: API_KEY, term: "surfboards"}, (videos)=>{
+            this.setState({videos});
+            // this.setState({videos: videos});
+        })
+    }
+    render(){
+        return(
+            <div>
+                <SearchBar />
+                <VideoList videos = {this.state.videos}/>
+                </div>
+        );
+    }
 }
+
 const rootDir = document.querySelector(".container")
 ReactDOM.render(<App />, rootDir);
